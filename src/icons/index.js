77252ff4753c51d564421d494811887c1e53fad1
Shreyansh Tiwari <111145568+Shreyannsh@users.noreply.github.com>
2024-05-22 00:26:27 +0530

    content added

--- src/icons/index.js
+++ src/icons/index.js
@@ -1,5 +1,7 @@
 import { ReactComponent as Logo } from "../assets/Connect logo LB 1.svg";
+import { ReactComponent as MouseScroll } from "../assets/iconoir_mouse-scroll-wheel.svg";
 
 export const ICONS = {
   logo: <Logo />,
+  mouseScroll: <MouseScroll />,
 };
--- src/pages/LandingPage/Landingpage.jsx
+++ src/pages/LandingPage/Landingpage.jsx
@@ -4,8 +4,20 @@ import { ICONS } from "../../icons";
 
 function LandingPage() {
   return (
-    <div className="logoDiv">
-      <div className="logo">{ICONS.logo}</div>
+    <div className="coverPage">
+      <div className="logoDiv">
+        <div className="logo">{ICONS.logo}</div>
+      </div>
+
+      <div className="coverPageBottom">
+        <p className="coverPageTextDiv">
+          <p className="coverPageText">
+            scroll down to discover All in one app - Explore the click of world
+            where dreams are fulfilled.
+          </p>
+        </p>
+        <div className="mouseIcon">{ICONS.mouseScroll}</div>
+      </div>
     </div>
   );
 }
