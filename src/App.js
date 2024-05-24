f3be08442d6c6520eb940ebdde22010fae3fb4f4
Shreyansh Tiwari <111145568+Shreyannsh@users.noreply.github.com>
2024-05-24 23:28:38 +0530

    created intro page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import LandingPage from "./pages/LandingPage/Landingpage";
 import SignupPage from "./pages/SignupPage/SignupPage";
+import Intro from "./pages/Intro/Intro";
 
 function App() {
   return (
     <div className="App">
       {/* <LandingPage /> */}
-      <SignupPage />
+      {/* <SignupPage /> */}
+      <Intro />
     </div>
   );
 }
---
+++ src/pages/Intro/Intro.jsx
@@ -0,0 +1,24 @@
+import "./Intro.css";
+
+const Intro = () => {
+  return (
+    <div>
+      <div className="topSection">
+        <div className="leftSection">
+          <h1>LOGO</h1>
+          <h2>Loreum ipsum solo et Loreum ipsum solo et.</h2>
+          <p className="text">
+            Egohet kaplatuna. Ana. Id. Psykotyp nötigt. Nenat. Isam. Teleligt
+            supravanade. Antere dekas. Reangar. Snackis but. Dekang trejon.
+            Virad. Diar naning. Donera avis. Lånegarderob nede. Deepfake
+            reaform. Tetisävis e-learning. Kroviling nyvis. Gardinhängarjobb
+            kyhet. Spement.
+          </p>
+        </div>
+        <div className="rightSection"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Intro;
